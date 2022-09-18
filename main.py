b18800c38996aba52e8a67aed579c6cbcba215ce
vldasha <davlasovskikh@hse.edu.ru>
2022-09-18 20:20:56 +0500

    реализованы функции нахождения числа фибоначчи рекурсивным и итеративным методом, а также нахождение определителя через строки

--- main.py
+++ main.py
@@ -9,17 +9,26 @@ def fibonacci_rec(num: int) -> int:
     :param num: the ordinal number of the fibonacci number
     :return: a fibonacci number
     """
-    pass
+    if num == 1:
+        return 0
+    if num == 2:
+        return 1
+    return fibonacci_rec(num-2)+fibonacci_rec(num-1)
 
+# print (fibonacci_rec(10))
 
-def fibonacci_iter(num: int) -> int:
+def fibonacci_iter (num: int) -> int:
     """Returns the fibonacci number according to the number specified in the
     parameter. Iterative implementation.
 
     :param num: the ordinal number of the fibonacci number
     :return: a fibonacci number
     """
-    pass
+    predpred, pred = 0, 1
+    for i in range (2, num+1):
+        predpred, pred = pred, predpred+pred
+    return predpred
+pass
 
 
 def determinant(matrix: [[int]]) -> int:
@@ -28,8 +37,22 @@ def determinant(matrix: [[int]]) -> int:
     :raise Exception: when the parameter value is not a square matrix
     :return: the value of the matrix determinant
     """
-    pass
+    size = len(matrix)
+    if (len(matrix) != len(matrix[0])):
+        raise Exception("not square")
+    for row in matrix:
+        if ((len(matrix) != len(row))):
+            raise Exception("jag matrix")
+    if size == 1:
+        return deter1x1(matrix)
+    return sum((-1)**j*matrix[0][j]*determinant(minor(matrix,0,j)) for j in range(size))
 
+def minor (matrix, i, j):
+    m=[row for r,row in enumerate(matrix)if r != i]
+    m=[col for c,col in enumerate(zip(*m))if c != j]
+    return m
+def deter1x1(matrix):
+    return matrix[0][0]
 
 def print_exec_time(func: callable(object), **kwargs: dict[str: Any]) -> None:
     start_time = time.time()
