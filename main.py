efa60c0d5f4a917ee8189a4e8cd94494128fd4a0
Akribob <spirit03breaker@mail.ru>
2022-09-20 15:38:35 +0500

    Нахождение чисел Фибоначчи итеративным и рекурсивным способами + нахождение определителя матрицы n-го порядка через разложение по строке

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import time
 from typing import Any
 
@@ -9,7 +10,14 @@ def fibonacci_rec(num: int) -> int:
     :param num: the ordinal number of the fibonacci number
     :return: a fibonacci number
     """
-    pass
+    if num <= 0:
+        raise Exception()
+    elif num == 1:
+        return 0
+    elif num == 2:
+        return 1
+    else:
+        return fibonacci_rec(num-1) + fibonacci_rec(num-2)
 
 
 def fibonacci_iter(num: int) -> int:
@@ -19,7 +27,21 @@ def fibonacci_iter(num: int) -> int:
     :param num: the ordinal number of the fibonacci number
     :return: a fibonacci number
     """
-    pass
+    if num <= 0:
+        raise Exception()
+    elif num == 1:
+        return 0
+    elif num == 2:
+        return 1
+    else:
+        a = 0
+        b = 1
+        c = 0
+        for i in range(num-2):
+            c = a+b
+            b = c
+            a = b - a
+        return c
 
 
 def determinant(matrix: [[int]]) -> int:
@@ -28,7 +50,59 @@ def determinant(matrix: [[int]]) -> int:
     :raise Exception: when the parameter value is not a square matrix
     :return: the value of the matrix determinant
     """
-    pass
+    if (matrix is None) or (not __matrix_is_square(matrix)):
+        raise Exception()
+    elif len(matrix) == 1:
+        return matrix[0][0]
+    elif len(matrix) == 2:
+        return __second_order(matrix)
+    else:
+        return __determinant_rec(matrix)
+
+
+def __matrix_is_square(matrix) -> bool:
+    """Проверяет, является ли матрица квадратной"""
+    columns = len(matrix)
+    for i in range(columns):
+        rows = len(matrix[i])
+        if rows != columns:
+            return False
+    return True
+
+
+def __second_order(matrix) -> int:
+    """ Вычисляет определитель матрицы 2х2 """
+    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+
+def __third_order(matrix) -> int:
+    """ Вычисляет определитель матрицы 3х3 """
+    return matrix[0][0] * matrix[1][1] * matrix[2][2] + matrix[0][1] * matrix[1][2] * matrix[2][0] + matrix[0][2] * matrix[1][0] * matrix[2][1] - matrix[0][2] * matrix[1][1] * matrix[2][0] - matrix[0][1] * matrix[1][0] * matrix[2][2] - matrix[0][0] * matrix[1][2] * matrix[2][1]
+
+
+def __determinant_rec(matrix) -> int:
+    """Считает определитель матрицы n-ого порядка"""
+    if len(matrix) == 2:
+        return __second_order(matrix)
+    else:
+        multipliers = matrix[0]
+        result = 0
+        character = 1
+        column_number = 0
+        for multiplier in multipliers:
+            result += character * multiplier * __determinant_rec(__smaller_matrix(matrix, 0, column_number))
+            character *= -1
+            column_number += 1
+        return result
+
+
+def __smaller_matrix(matrix: [[int]], row_number, column_number) -> [[int]]:
+    """удаляет строку и столбец из матрицы"""
+    matrix_copy = copy.deepcopy(matrix)
+    matrix_copy.pop(row_number)
+    for i in matrix_copy:
+        i.pop(column_number)
+    return matrix_copy
 
 
 def print_exec_time(func: callable(object), **kwargs: dict[str: Any]) -> None:
