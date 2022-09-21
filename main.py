6f51ae045b19674fd4ac851103bce54183d62f3a
Амина Хисматова <aakhismatova@edu.hse.ru>
2022-09-22 01:23:51 +0500

    Группа Orange. Выполнено задание 1 нахождение n-ого числа последовательности Фибоначчи и определителя матрицы

--- main.py
+++ main.py
@@ -3,32 +3,54 @@ from typing import Any
 
 
 def fibonacci_rec(num: int) -> int:
-    """Returns the fibonacci number according to the number specified in the
-    parameter. Recursive implementation.
-
-    :param num: the ordinal number of the fibonacci number
-    :return: a fibonacci number
-    """
+    if num <= 0:
+        return -1
+    elif num == 1:
+        return 0
+    elif num == 2:
+        return 1
+    else:
+        return fibonacci_rec(num - 1) + fibonacci_rec(num - 2)
     pass
 
 
 def fibonacci_iter(num: int) -> int:
-    """Returns the fibonacci number according to the number specified in the
-    parameter. Iterative implementation.
-
-    :param num: the ordinal number of the fibonacci number
-    :return: a fibonacci number
-    """
+    a = 0
+    b = 1
+    if num <= 0:
+        return -1
+    elif num == 1:
+        return a
+    elif num == 2:
+        return b
+    else:
+        for i in range(3, num + 1):
+            c = a + b
+            a = b
+            b = c
+        return b
     pass
 
 
 def determinant(matrix: [[int]]) -> int:
-    """Calculates the value of the matrix determinant
-    :param matrix: an integer matrix
-    :raise Exception: when the parameter value is not a square matrix
-    :return: the value of the matrix determinant
-    """
-    pass
+    i = list(range(len(matrix)))
+    result = 0
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2 and len(matrix[0]) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+    for f in i:
+        matrix1 = matrix
+        matrix1 = matrix1[1:]
+        h = len(matrix1)
+        for ind in range(h):
+            matrix1[ind] = matrix1[ind][0:f] + matrix1[ind][f + 1:]
+        sign = (-1) ** (f % 2)
+        result += sign * matrix[0][f] * determinant(matrix1)
+    return result
+
+
+pass
 
 
 def print_exec_time(func: callable(object), **kwargs: dict[str: Any]) -> None:
