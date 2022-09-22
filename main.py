1cd80e6e63019f19ea8949ebaf2ce8386b9052b1
Akribob <spirit03breaker@mail.ru>
2022-09-22 14:34:51 +0500

    Сделаны задания НОК и НОД

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def gcd_recursive(a: int, b: int) -> int:
     """
     if a is None or b is None:
         raise Exception()
-    if a*b == 0:
+    elif a*b == 0:
         return a+b
-    if a == b:
+    elif a == b:
         return a
     return gcd_recursive(max(a, b) - min(a, b), min(a, b))
 
@@ -26,9 +26,13 @@ def gcd_iterative_slow(a: int, b: int) -> int:
     :return: greatest common divisor
     """
     if a is None or b is None:
-        raise Exception()
-    while a != b or a*b != 0:
-        max(a, b) - min(a, b)
+        raise Exception("Ошибка ввода")
+    elif a == 0 or b == 0:
+        raise Exception("a или b равняется 0")
+    while a != b:
+        if a < b:
+            a, b = b, a
+        a -= b
     return max(a, b)
 
 
@@ -41,13 +45,10 @@ def gcd_iterative_fast(a: int, b: int) -> int:
     :except Exception: when a or b value is None
     :return: greatest common divisor
     """
-    # остаток от целочисленного деления
     if a is None or b is None:
         raise Exception()
     while b != 0:
-        a %= b
-        a, b = b, a
-        #a, b = b, a%b
+        a, b = b, a % b
     return a
 
 
@@ -59,7 +60,6 @@ def lcm(a: int, b: int) -> int:
     :except Exception: when a or b value is None
     :return: the least common multiple
     """
-    # нок = а*b / нод
     return int(a*b / gcd_iterative_fast(a, b))
 
 
