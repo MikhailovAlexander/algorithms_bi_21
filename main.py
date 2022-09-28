b4bf362f459b186d611d0e9c92f0a5824505564f
HSEpinkteam <garaeva.milanaa@gmail.com>
2022-09-28 23:50:48 +0500

    pinkteam_task2_python

--- main.py
+++ main.py
@@ -1,48 +1,36 @@
 def gcd_recursive(a: int, b: int) -> int:
-    """Calculates the greatest common divisor of two numbers.
-    Recursive implementation.
-
-    :param a: first number
-    :param b: second number
-    :except Exception: when a or b value is None
-    :return: greatest common divisor
-    """
-    pass
+    if a is None or b is None:
+        raise Exception
+    if a == 0 or b == 0:
+        return a + b
+    if a == b:
+        return a
+    if a < b:
+        a, b = b, a
+    a -= b
+    return gcd_recursive(a, b)
 
 
 def gcd_iterative_slow(a: int, b: int) -> int:
-    """Calculates the greatest common divisor of two numbers.
-    Iterative implementation using subtraction.
-
-    :param a: first number
-    :param b: second number
-    :except Exception: when a or b value is None
-    :return: greatest common divisor
-    """
-    pass
+    if a is None or b is None:
+        raise Exception
+    while not (a == 0 or b == 0) and a != b:
+        if a < b:
+            a, b = b, a
+        a -= b
+    return a
 
 
 def gcd_iterative_fast(a: int, b: int) -> int:
-    """Calculates the greatest common divisor of two numbers
-    Iterative implementation using division.
-
-    :param a: first number
-    :param b: second number
-    :except Exception: when a or b value is None
-    :return: greatest common divisor
-    """
-    pass
+    if a is None or b is None:
+        raise Exception
+    while b:
+        a, b = b, a % b
+    return a
 
 
 def lcm(a: int, b: int) -> int:
-    """Calculates the least common multiple of two numbers
-
-    :param a: first number
-    :param b: second number
-    :except Exception: when a or b value is None
-    :return: the least common multiple
-    """
-    pass
+    return int(a * b / gcd_iterative_fast(a, b))
 
 
 def main():
