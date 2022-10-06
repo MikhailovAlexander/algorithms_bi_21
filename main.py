f9199ab75e25a14944a2b7729cc46e6e90c657c8
Akribob <spirit03breaker@mail.ru>
2022-10-06 20:57:32 +0500

    недоделанный путь

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class ArgumentException(Exception):
     """Exception raised for errors in the input parameter.
 
@@ -19,7 +22,49 @@ def get_min_cost_path(price_table: list[list[float]]) ->\
     :return: a dictionary with keys: cost - the minimum value of the cost of the
     path, path - an ordered list of tuples with cell indices.
     """
+    #проверка
+    costs_table = __calculate_costs(price_table)
     pass
+
+
+def __calculate_costs(price_table) -> list[list[float]]:
+    matrix_copy = copy.deepcopy(price_table)
+
+    length = len(matrix_copy[0])
+    new_row = [float("inf")]
+    for i in range(length-1):
+        new_row.append(float("inf"))
+    matrix_copy.insert(0, new_row)
+
+    for row in matrix_copy:
+        row.insert(0, float("inf"))
+
+    count_row = len(price_table)
+    count_column = len(price_table[0])
+
+    for i in range(count_row):
+        for j in range(count_column):
+            if (i == 0) and (j == 0):
+                matrix_copy[i+1][j+1] = price_table[i][j]
+
+            if matrix_copy[i+1][j] < matrix_copy[i][j+1]:
+                matrix_copy[i+1][j+1] += matrix_copy[i+1][j]
+            else:
+                matrix_copy[i+1][j+1] += matrix_copy[i][j+1]
+
+    path = [(count_row, count_column)]
+    while count_row != 1 or count_column != 1:
+        if matrix_copy[count_row-1][count_column] < matrix_copy[count_row][count_column-1]:
+            path.insert(0, (count_row-2, count_column-1))
+            count_row -= 1
+        else:
+            path.insert(0, (count_row-1, count_column - 2))
+            count_column -= 1
+
+
+
+    print(path)
+    return matrix_copy
 
 
 def main():
