14823c91e27b119801d640f6ee7696f637fca020
Akribob <spirit03breaker@mail.ru>
2022-11-14 18:54:33 +0500

    без проверок

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import copy
+
 from custom_exception import ArgumentException
 
 
@@ -7,7 +9,36 @@ def tridiagonal_determinant(matrix: list[list[int]]) -> int:
     :raise ArgumentException: when parameter is not a tridiagonal integer matrix
     :return: the value of the matrix determinant
     """
-    pass
+
+
+    return __determinant_rec(matrix)
+
+
+def __determinant_rec(matrix) -> int:
+    """Считает определитель матрицы n-ого порядка"""
+    if len(matrix) == 1:
+        return matrix[0][0]
+    elif len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+    else:
+        multipliers = matrix[0]
+        result = 0
+        character = 1
+        column_number = 0
+        for multiplier in multipliers:
+            result += character * multiplier * __determinant_rec(__smaller_matrix(matrix, 0, column_number))
+            character *= -1
+            column_number += 1
+        return result
+
+
+def __smaller_matrix(matrix: [[int]], row_number, column_number) -> [[int]]:
+    """удаляет строку и столбец из матрицы"""
+    matrix_copy = copy.deepcopy(matrix)
+    matrix_copy.pop(row_number)
+    for i in matrix_copy:
+        i.pop(column_number)
+    return matrix_copy
 
 
 def main():
