601469155ac8044d19f41bab4a30769f79fa99c8
Akribob <spirit03breaker@mail.ru>
2022-11-22 20:38:40 +0500

    пути

--- main.py
+++ main.py
@@ -10,7 +10,31 @@ def get_triangle_path_count(length: int) -> int:
     greater than 0
     :return: the number of routes.
     """
-    pass
+    if not __check_input(length):
+        raise ArgumentException('The parameter length must be an integer greater than 0')
+    return get_a(length)
+
+
+def __check_input(n) -> bool:
+    return not (n is None or not(type(n) is int) or n < 1)
+
+
+def get_a(n):
+    if n == 1:
+        return 0
+    return get_b(n-1) + get_c(n-1)
+
+
+def get_b(n):
+    if n == 1:
+        return 1
+    return get_a(n-1) + get_c(n-1)
+
+
+def get_c(n):
+    if n == 1:
+        return 1
+    return get_b(n-1) + get_a(n-1)
 
 
 def generate_strings(length: int) -> list[str]:
@@ -20,6 +44,17 @@ def generate_strings(length: int) -> list[str]:
     :raise ArgumentException: when integer is not equal or greater than zero.
     :return: the list of strings consisting zeroes and ones.
     """
+    output = []
+    __add1(output, length)
+    __add0(output, length)
+    return output
+
+
+def __add1(string, length_needed) -> str:
+    pass
+
+
+def __add0(string, length) -> list[str]:
     pass
 
 
