375af8451e7d24e70ae1b3da584bde7a02295d67
GroupOrange <aakshismatova@edu.hse.ru>
2022-11-23 21:30:07 +0500

    для теста 2

--- main.py
+++ main.py
@@ -10,7 +10,27 @@ def get_triangle_path_count(length: int) -> int:
     greater than 0
     :return: the number of routes.
     """
-    if
+    is_ok(length)
+    return point_a(length)
+def point_a(n):
+    if n == 1:
+        return 0
+    return point_b(n-1) + point_c(n-1)
+
+
+def point_b(n):
+    if n == 1:
+        return 1
+    return point_a(n-1) + point_c(n-1)
+
+
+def point_c(n):
+    if n == 1:
+        return 1
+    return point_b(n-1) + point_a(n-1)
+def is_ok(length: int) -> bool:
+    if type(length) != int or length < 1 or length is None:
+        raise ArgumentException('The parameter length must be an integer greater than 0')
 
 
 def generate_strings(length: int) -> list[str]:
@@ -20,25 +40,22 @@ def generate_strings(length: int) -> list[str]:
     :raise ArgumentException: when integer is not equal or greater than zero.
     :return: the list of strings consisting zeroes and ones.
     """
-    if length <= 0 or length is None or type(length) != int:
-        raise ArgumentException('The parameter length must be an integer greater than 0')
-    array=[]
-    __set_1('',length,array), __set_0('',length,array)
+    is_ok(length)
+    array = []
+    __set_1("",length,array)
+    __set_0("",length,array)
     return array
-def __set_1(string,length,array)
-    if len(string) == length:
-        array.append(string)
-    else:
-        string += '1'
-    __set_1(string, length, array)
-    __set_0(string, length, array)
+def __set_1(string,length,array):
+    if len(string)+1 == length:
+        return array.append(string+"1")
+    __set_1(string+"1", length, array)
+    __set_0(string+"1", length, array)
 
-def __set_0(string,length,array)
-    if len(string) == length:
-        array.append(string)
-    else:
-        string += '0'
-    __set_1(string, length, array)
+def __set_0(string,length,array):
+    if len(string)+1 == length:
+        return array.append(string+"0")
+    __set_1(string+"0", length, array)
+
 def main():
     print(get_triangle_path_count(4))
     print(generate_strings(5))
