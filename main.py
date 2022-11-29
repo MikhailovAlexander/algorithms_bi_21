b0fab9b52f496da000696ad1187ba3c36fdc648a
kattux <eetsarkova@edu.hse.ru>
2022-11-29 19:51:22 +0500

    red_team_solution

--- main.py
+++ main.py
@@ -10,7 +10,29 @@ def get_triangle_path_count(length: int) -> int:
     greater than 0
     :return: the number of routes.
     """
-    pass
+    IsCorrectInput(length)
+    return par_a(length)
+
+def IsCorrectInput (length: int) -> bool:
+    if type(length) is not int or length < 1 or length is None:
+        raise ArgumentException('The parameter length must be an integer greater than 0')
+
+def par_a(n):
+    if n == 1:
+        return 0
+    return par_b(n-1) + par_c(n-1)
+
+
+def par_b(n):
+    if n == 1:
+        return 1
+    return par_a(n-1)+par_c(n-1)
+
+
+def par_c(n):
+    if n == 1:
+        return 1
+    return par_b(n-1)+par_a(n-1)
 
 
 def generate_strings(length: int) -> list[str]:
@@ -20,7 +42,24 @@ def generate_strings(length: int) -> list[str]:
     :raise ArgumentException: when integer is not equal or greater than zero.
     :return: the list of strings consisting zeroes and ones.
     """
-    pass
+    IsCorrectInput(length)
+    array = []
+    add_1('', length, array)
+    add_0('', length, array)
+    return array
+
+
+def add_1(string, n, array):
+    if len(string) + 1 == n:
+        return array.append(string + '1')
+    add_1(string + '1', n, array)
+    add_0(string + '1', n, array)
+
+
+def add_0(string, n, array):
+    if len(string) + 1 == n:
+        return array.append(string + '0')
+    add_1(string + '0', n, array)
 
 
 def main():
